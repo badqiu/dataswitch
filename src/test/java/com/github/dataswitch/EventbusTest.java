a1944ae720bd52f843bb69a5caaa15a28996e0fe
badqiu <badqiu@gmail.com>
2015-08-21 10:35:06 +0800

    1

---
+++ src/test/java/com/github/dataswitch/EventbusTest.java
@@ -0,0 +1,40 @@
+package com.github.dataswitch;
+
+import java.util.concurrent.Executors;
+
+import org.junit.Test;
+
+import com.google.common.eventbus.AsyncEventBus;
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+
+public class EventbusTest {
+
+	@Test
+	public void test() throws InterruptedException {
+//		EventBus eventbus = new EventBus();
+		EventBus eventbus = new AsyncEventBus(Executors.newFixedThreadPool(2));
+		eventbus.register(new NumListener());
+		
+		for(int i = 0; i < 100; i++) {
+			eventbus.post(new Long(i));
+			eventbus.post(new Integer(i));
+		}
+		
+		Thread.sleep(500);
+	}
+	
+	public static class NumListener {
+		
+		@Subscribe
+		public void listenLong(Long v) {
+			System.out.println(Thread.currentThread().getId() + " listenLong,v="+v);
+		}
+		
+		@Subscribe
+		public void listenInteger(Integer v) {
+			System.out.println(Thread.currentThread().getId() + " listenInteger,v="+v);
+		}
+		
+	}
+}
