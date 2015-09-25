bddac6388540ea9f7ab4c94232d3305b0b309e84
badqiu <badqiu@gmail.com>
2015-09-25 14:50:12 +0800

    1

---
+++ src/test/java/com/github/dataswitch/util/InputsOutputsTest.java
@@ -0,0 +1,13 @@
+package com.github.dataswitch.util;
+
+import org.junit.Test;
+
+public class InputsOutputsTest {
+
+	@Test
+	public void test() {
+		InputsOutputs job = new InputsOutputs();
+		job.exec();
+	}
+
+}
