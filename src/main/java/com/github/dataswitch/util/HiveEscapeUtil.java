82b963ad9e60d319a8c38fb8a5cfba6870f8a6d8
badqiu <badqiu@gmail.com>
2017-10-12 10:31:13 +0800

    1

--- src/main/java/com/github/dataswitch/util/HiveEscapeUtil.java
+++ src/main/java/com/github/dataswitch/util/HiveEscapeUtil.java
@@ -8,9 +8,9 @@ public class HiveEscapeUtil {
 		if(str == null) return null;
 		
 		String result = StringUtils.replace(str, "\n", "\\n");
-		result = StringUtils.replace(result, "\001", "\\001");
-		result = StringUtils.replace(result, "\002", "\\002");
-		result = StringUtils.replace(result, "\003", "\\003");
+//		result = StringUtils.replace(result, "\001", "\\001");
+//		result = StringUtils.replace(result, "\002", "\\002");
+//		result = StringUtils.replace(result, "\003", "\\003");
 		return result;
 	}
 	
