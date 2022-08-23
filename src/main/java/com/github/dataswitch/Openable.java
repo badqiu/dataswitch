16be6b8de5e60a93021f1d1c902aaaa9ba93a111
badqiu <badqiu@qq.com>
2022-08-23 18:43:20 +0800

    1

--- src/main/java/com/github/dataswitch/Openable.java
+++ src/main/java/com/github/dataswitch/Openable.java
@@ -2,7 +2,7 @@ package com.github.dataswitch;
 
 import java.util.Map;
 
-/** 开始执行之前 */
+/** 开始执行之前执行 */
 public interface Openable {
 
 	public void open(Map<String,Object> params) throws Exception;
