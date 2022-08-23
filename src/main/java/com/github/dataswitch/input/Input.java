2f726c765c2f0c596ee6df60aa532aa4a318eec8
badqiu <badqiu@qq.com>
2022-08-23 17:14:19 +0800

    1

--- src/main/java/com/github/dataswitch/input/Input.java
+++ src/main/java/com/github/dataswitch/input/Input.java
@@ -8,7 +8,7 @@ public interface Input extends Closeable{
 	/**
 	 * 读取数据，
 	 * @param size 每次读取批量大小
-	 * @return 返回为null或空，代理读取结束，程序退出
+	 * @return 返回为null或空，代表读取数据结束，程序可以退出
 	 */
 	public List<Object> read(int size) ;
 	
