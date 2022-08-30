c2fe20560146051311fe602f6ecd8890292f8b6d
badqiu <badqiu@qq.com>
2022-08-30 11:02:17 +0800

    retry

--- src/main/java/com/github/dataswitch/output/RetryOutput.java
+++ src/main/java/com/github/dataswitch/output/RetryOutput.java
@@ -2,12 +2,15 @@ package com.github.dataswitch.output;
 
 import java.util.List;
 
+import org.springframework.util.Assert;
+
 import com.github.rapid.common.util.Retry;
 
 public class RetryOutput extends ProxyOutput {
 	
-	private int retryTimes = 5;
-	private int retryIntervalSeconds = 3;
+	private int retryTimes = 0; //重试次数
+	private long retryIntervalMills = 3000; //重试间隔(毫秒)
+	private long retryTimeoutMills = 0; //重试超时时间
 
 	public RetryOutput() {
 	}
@@ -21,22 +24,39 @@ public class RetryOutput extends ProxyOutput {
 	}
 
 	public void setRetryTimes(int retryTimes) {
+		Assert.isTrue(retryTimes > 0 ,"retryTimes > 0 must be true");
 		this.retryTimes = retryTimes;
 	}
 
-	public int getRetryIntervalSeconds() {
-		return retryIntervalSeconds;
+	public void setRetryIntervalMinutes(long value) {
+		setRetryIntervalMills(value * 1000 * 60);
+	}
+	
+	public void setRetryIntervalSeconds(long value) {
+		setRetryIntervalMills(value * 1000);
+	}
+	
+	public void setRetryIntervalMills(long retryIntervalMills) {
+		Assert.isTrue(retryIntervalMills > 0 ,"retryIntervalMills > 0 must be true");
+		this.retryIntervalMills = retryIntervalMills;
+	}
+	
+	public void setRetryTimeoutMills(long retryTimeoutMills) {
+		this.retryTimeoutMills = retryTimeoutMills;
 	}
 
-	public void setRetryIntervalSeconds(int retryIntervalSeconds) {
-		this.retryIntervalSeconds = retryIntervalSeconds;
+	public void setRetryTimeoutSeconds(long value) {
+		setRetryTimeoutMills(value * 1000);
 	}
-
+	
+	public void setRetryTimeoutMinutes(long value) {
+		setRetryTimeoutMills(value * 1000 * 60);
+	}
+	
 	@Override
 	public void write(List<Object> rows) {
 
-		int retryIntervalMills = retryIntervalSeconds * 1000;
-		Retry.retry(retryTimes, retryIntervalMills, () -> {
+		Retry.retry(retryTimes, retryIntervalMills,retryTimeoutMills, () -> {
 			super.write(rows);
 			return null;
 		});
