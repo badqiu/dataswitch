3cbeb5a75072950530be5c7383a6bfec39b90b88
badqiu <badqiu@qq.com>
2022-08-31 14:14:55 +0800

    queue

--- src/main/java/com/github/dataswitch/util/QueueProvider.java
+++ src/main/java/com/github/dataswitch/util/QueueProvider.java
@@ -6,14 +6,21 @@ import java.util.concurrent.BlockingQueue;
 
 import org.springframework.util.Assert;
 
+import com.github.dataswitch.Constants;
 import com.github.dataswitch.Openable;
 
 public class QueueProvider implements Openable {
-	private int queueSize = 100;
+	private int queueSize = 100000 / Constants.DEFAULT_BUFFER_SIZE;
 	
 	private String queueGroup = "default";
 	private String queueName;
+	
 	private BlockingQueue<List<Object>> queue = null;
+	
+	@Override
+	public void open(Map<String, Object> params) throws Exception {
+		init();
+	}
 	
 	public void init() {
 		
@@ -26,13 +33,32 @@ public class QueueProvider implements Openable {
 		
 	}
 	
-	public BlockingQueue newBlockingQueue(String queueId,int queueSize) {
+	protected BlockingQueue newBlockingQueue(String queueId,int queueSize) {
 		return QueueUtil.getBlockingQueue(queueId, queueSize);
 	}
 	
-	@Override
-	public void open(Map<String, Object> params) throws Exception {
-		init();
+	public int getQueueSize() {
+		return queueSize;
+	}
+
+	public void setQueueSize(int queueSize) {
+		this.queueSize = queueSize;
+	}
+
+	public String getQueueGroup() {
+		return queueGroup;
+	}
+
+	public void setQueueGroup(String queueGroup) {
+		this.queueGroup = queueGroup;
+	}
+
+	public String getQueueName() {
+		return queueName;
+	}
+
+	public void setQueueName(String queueName) {
+		this.queueName = queueName;
 	}
 
 	public BlockingQueue<List<Object>> getQueue() {
--- src/main/java/com/github/dataswitch/util/QueueUtil.java
+++ src/main/java/com/github/dataswitch/util/QueueUtil.java
@@ -10,13 +10,15 @@ import org.springframework.util.Assert;
 
 public class QueueUtil {
 
-	static Map<String,BlockingQueue> queueMap = new HashMap<String,BlockingQueue>();
+	private static Map<String,BlockingQueue> queueMap = new HashMap<String,BlockingQueue>();
 	
 	public static synchronized BlockingQueue getBlockingQueue(String queueId,int queueSize) {
 		Assert.hasText(queueId,"queueId must be not blank");
+		Assert.isTrue(queueSize > 0,"queueSize > 0 must be true");
+		
 		BlockingQueue queue = queueMap.get(queueId);
 		if(queue == null) {
-			queue = new ArrayBlockingQueue<List<Object>>(queueSize);
+			queue = new ArrayBlockingQueue(queueSize);
 			queueMap.put(queueId, queue);
 		}
 		return queue;
