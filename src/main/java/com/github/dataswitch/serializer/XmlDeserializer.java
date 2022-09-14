f2f866686fae4a266386c83b34064edca8dda93a
badqiu <badqiu@qq.com>
2022-09-14 17:35:27 +0800

    xml

--- src/main/java/com/github/dataswitch/serializer/XmlDeserializer.java
+++ src/main/java/com/github/dataswitch/serializer/XmlDeserializer.java
@@ -6,12 +6,15 @@ import java.io.InputStream;
 import org.springframework.core.serializer.Deserializer;
 
 import com.github.dataswitch.BaseObject;
+import com.thoughtworks.xstream.XStream;
 
 public class XmlDeserializer extends BaseObject implements Deserializer{
-
+	static XStream xstream = new XStream();
+	
 	@Override
 	public Object deserialize(InputStream inputStream) throws IOException {
-		return null;
+		Object result = xstream.fromXML(inputStream);
+		return result;
 	}
 
 }
--- src/main/java/com/github/dataswitch/serializer/XmlSerializer.java
+++ src/main/java/com/github/dataswitch/serializer/XmlSerializer.java
@@ -4,11 +4,18 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 import com.github.dataswitch.BaseObject;
+import com.thoughtworks.xstream.XStream;
 
 public class XmlSerializer extends BaseObject implements Serializer{
 
+	static XStream xstream = new XStream();
+	
 	@Override
 	public void serialize(Object object, OutputStream outputStream) throws IOException {
+		if(object == null) return;
+		
+		xstream.toXML(object, outputStream);
+		outputStream.write("\n".getBytes());
 		
 	}
 
