8c41d9479ce35942556254519f112fdf6788947e
badqiu <badqiu@qq.com>
2022-10-20 16:40:38 +0800

    1

---
+++ src/main/java/com/github/dataswitch/support/LockProvider.java
@@ -0,0 +1,67 @@
+package com.github.dataswitch.support;
+
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.function.BiFunction;
+
+import com.github.dataswitch.Openable;
+import com.github.dataswitch.enums.Constants;
+import com.github.dataswitch.util.LockUtil;
+
+public class LockProvider implements Openable {
+
+	private String lockGroup = Constants.DEFAULT_LOCK_GROUP;
+	private String lockId;
+	
+	private Lock lock;
+	
+	private BiFunction<String, String, Lock> newLockFunction;
+
+	public String getLockGroup() {
+		return lockGroup;
+	}
+	
+	public void setLockGroup(String lockGroup) {
+		this.lockGroup = lockGroup;
+	}
+	
+	public String getLockId() {
+		return lockId;
+	}
+	
+	public void setLockId(String lockId) {
+		this.lockId = lockId;
+	}
+	
+	public Lock getLock() {
+		return lock;
+	}
+
+	public void setLock(Lock lock) {
+		this.lock = lock;
+	}
+	
+	public BiFunction<String, String, Lock> getNewLockFunction() {
+		return newLockFunction;
+	}
+
+	public void setNewLockFunction(BiFunction<String, String, Lock> newLockFunction) {
+		this.newLockFunction = newLockFunction;
+	}
+
+	protected Lock newLock(String lockGroup,String lockId) {
+		if(newLockFunction == null) {
+			return LockUtil.getReentrantLock(lockGroup, lockId);
+		}else {
+			return newLockFunction.apply(lockGroup, lockId);
+		}
+	}
+
+	@Override
+	public void open(Map<String, Object> params) throws Exception {
+		if(lock == null) {
+			lock = newLock(lockGroup,lockId);
+		}
+	}
+	
+}
