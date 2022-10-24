ee20ed37b28c60b866e357ca7544281028153e71
badqiu <badqiu@qq.com>
2022-10-24 16:36:26 +0800

    1

--- src/main/java/com/github/dataswitch/enums/FailMode.java
+++ src/main/java/com/github/dataswitch/enums/FailMode.java
@@ -3,19 +3,27 @@ package com.github.dataswitch.enums;
 import org.apache.commons.lang.StringUtils;
 
 public enum FailMode {
-	FAIL_FAST("failFast"),FAIL_AT_END("failAtEnd"),FAIL_NEVER("failNever");
+	FAIL_FAST("failFast","快速失败"),
+	FAIL_AT_END("failAtEnd","结束时失败"),
+	FAIL_NEVER("failNever","从不失败");
 	
 
 	private final String shortName;
+	private final String desc;
 	
-	FailMode(String shortName) {
+	FailMode(String shortName,String desc) {
 		this.shortName = shortName;
+		this.desc = desc;
 	}
 
 	public String getShortName() {
 		return shortName;
 	}
 	
+	public String getDesc() {
+		return desc;
+	}
+
 	public static FailMode getRequiredByName(String name) {
 		if(StringUtils.isBlank(name)) {
 			return null;
