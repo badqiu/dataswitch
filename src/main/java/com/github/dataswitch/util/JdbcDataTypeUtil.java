6e9dd26ef70bb011ddb857094c1744fae2ee2aeb
badqiu <badqiu@qq.com>
2022-10-27 10:32:47 +0800

    1

--- src/main/java/com/github/dataswitch/util/JdbcDataTypeUtil.java
+++ src/main/java/com/github/dataswitch/util/JdbcDataTypeUtil.java
@@ -6,10 +6,12 @@ import org.springframework.util.Assert;
 
 public class JdbcDataTypeUtil {
 
+	private static final String VARCHAR = "VARCHAR(4000)";
+
 	public static String getDatabaseDataType(String url,Object value) {
 		Assert.hasText(url,"jdbc url must be not blank");
 		
-		if(url.contains("mysql") || url.contains("mariadb")) {
+		if(isMysqlJdbcUrl(url)) {
 			return JdbcDataTypeUtil.getMysqlDataType(value);
 		}else if(url.contains("clickhouse")) {
 			return JdbcDataTypeUtil.getClickHouseDataType(value);
@@ -29,6 +31,10 @@ public class JdbcDataTypeUtil {
 			throw new UnsupportedOperationException("cannot get database type by url:"+url);
 		}
 	}
+
+	public static boolean isMysqlJdbcUrl(String url) {
+		return url.contains("mysql") || url.contains("mariadb");
+	}
 	
     public static String getClickHouseDataType(Object value) {
         if (value == null) {
@@ -39,12 +45,9 @@ public class JdbcDataTypeUtil {
             return "String";
         }
 
-		if(value instanceof Double) return "Float64";
-		if(value instanceof Float) return "Float32";
-		if(value.getClass() == double.class) return "Float64";
-		if(value.getClass() == float.class) return "Float32";
+        if(isDoubleNumber(value)) return "Float64";
 		
-		if(value instanceof Number) return "Int64";
+		if(value instanceof Number || value instanceof Boolean) return "Int64";
 		if(value instanceof Date) return "DateTime64(3)";
 
         return "String";
@@ -52,82 +55,70 @@ public class JdbcDataTypeUtil {
     
     public static String getMysqlDataType(Object value) {
         if (value == null) {
-            return "varchar(200)";
+            return VARCHAR;
         }
 
         if (value instanceof String) {
-            return "varchar(200)";
+            return VARCHAR;
         }
 
-		if(value instanceof Double) return "DOUBLE";
-		if(value instanceof Float) return "FLOAT";
-		if(value.getClass() == double.class) return "DOUBLE";
-		if(value.getClass() == float.class) return "FLOAT";
+        if(isDoubleNumber(value)) return "DOUBLE";
 		
-		if(value instanceof Number) return "BIGINT";
+		if(value instanceof Number || value instanceof Boolean) return "BIGINT";
 		if(value instanceof Date) return "DATETIME";
 
-        return "varchar(200)";
+        return VARCHAR;
     }
     
     public static String getOracleDataType(Object value) {
         if (value == null) {
-            return "VARCHAR2(200)";
+            return VARCHAR;
         }
 
         if (value instanceof String) {
-            return "VARCHAR2(200)";
+            return VARCHAR;
         }
 
-		if(value instanceof Double) return "FLOAT";
-		if(value instanceof Float) return "FLOAT";
-		if(value.getClass() == double.class) return "FLOAT";
-		if(value.getClass() == float.class) return "FLOAT";
+        if(isDoubleNumber(value)) return "NUMBER(38,4)";
 		
-		if(value instanceof Number) return "INTEGER";
+		if(value instanceof Number || value instanceof Boolean) return "NUMBER(38,0)";
 		if(value instanceof Date) return "DATE";
 
-        return "VARCHAR2(200)";
+        return VARCHAR;
     }
     
     public static String getSqlServerDataType(Object value) {
         if (value == null) {
-            return "VARCHAR(200)";
+            return VARCHAR;
         }
 
         if (value instanceof String) {
-            return "VARCHAR(200)";
+            return VARCHAR;
         }
 
-		if(value instanceof Double) return "FLOAT";
-		if(value instanceof Float) return "FLOAT";
-		if(value.getClass() == double.class) return "FLOAT";
-		if(value.getClass() == float.class) return "FLOAT";
+        if(isDoubleNumber(value)) return "FLOAT";
 		
-		if(value instanceof Number) return "BIGINT";
+		if(value instanceof Number || value instanceof Boolean) return "BIGINT";
 		if(value instanceof Date) return "DATETIME";
 
-        return "VARCHAR(200)";
+        return VARCHAR;
     }
     
     public static String getPostgreSQLDataType(Object value) {
         if (value == null) {
-            return "VARCHAR(200)";
+            return VARCHAR;
         }
 
         if (value instanceof String) {
-            return "VARCHAR(200)";
+            return VARCHAR;
         }
 
-		if(value instanceof Double) return "float8";
-		if(value instanceof Float) return "float8";
-		if(value.getClass() == double.class) return "float8";
-		if(value.getClass() == float.class) return "float8";
+        if(isDoubleNumber(value)) return "float8";
 		
-		if(value instanceof Number) return "BIGINT";
+		if(value instanceof Number || value instanceof Boolean) return "BIGINT";
 		if(value instanceof Date) return "timestamp";
 
-        return "VARCHAR(200)";
+        return VARCHAR;
     }
 
 	public static String getHiveDataType(Object value) {
@@ -139,12 +130,9 @@ public class JdbcDataTypeUtil {
             return "STRING";
         }
 
-		if(value instanceof Double) return "DOUBLE";
-		if(value instanceof Float) return "FLOAT";
-		if(value.getClass() == double.class) return "DOUBLE";
-		if(value.getClass() == float.class) return "FLOAT";
+        if(isDoubleNumber(value)) return "DOUBLE";
 		
-		if(value instanceof Number) return "BIGINT";
+		if(value instanceof Number || value instanceof Boolean) return "BIGINT";
 		if(value instanceof Date) return "TIMESTAMP";
 
         return "STRING";
@@ -152,61 +140,63 @@ public class JdbcDataTypeUtil {
     
 	public static String getH2DataType(Object value) {
         if (value == null) {
-            return "VARCHAR(200)";
+            return VARCHAR;
         }
 
         if (value instanceof String) {
-            return "VARCHAR(200)";
+            return VARCHAR;
         }
 
-		if(value instanceof Double) return "NUMERIC(20, 2)";
-		if(value instanceof Float) return "NUMERIC(20, 2)";
-		if(value.getClass() == double.class) return "NUMERIC(20, 2)";
-		if(value.getClass() == float.class) return "NUMERIC(20, 2)";
+        if(isDoubleNumber(value)) return "NUMERIC(20, 2)";
 		
-		if(value instanceof Number) return "BIGINT";
+		if(value instanceof Number || value instanceof Boolean) return "BIGINT";
 		if(value instanceof Date) return "TIMESTAMP";
 
-        return "VARCHAR(200)";		
+        return VARCHAR;		
 	}
 
 	public static String getHSQLDataType(Object value) {
         if (value == null) {
-            return "VARCHAR(200)";
+            return VARCHAR;
         }
 
         if (value instanceof String) {
-            return "VARCHAR(200)";
+            return VARCHAR;
         }
 
-		if(value instanceof Double) return "REAL";
-		if(value instanceof Float) return "REAL";
-		if(value.getClass() == double.class) return "REAL";
-		if(value.getClass() == float.class) return "REAL";
+        if(isDoubleNumber(value)) return "REAL";
 		
-		if(value instanceof Number) return "BIGINT";
+		if(value instanceof Number || value instanceof Boolean) return "BIGINT";
 		if(value instanceof Date) return "DATETIME";
 
-        return "VARCHAR(200)";		
+        return VARCHAR;		
 	}
 	
 	public static String getSybaseDataType(Object value) {
         if (value == null) {
-            return "VARCHAR(200)";
+            return VARCHAR;
         }
 
         if (value instanceof String) {
-            return "VARCHAR(200)";
+            return VARCHAR;
         }
 
-		if(value instanceof Double) return "REAL";
-		if(value instanceof Float) return "REAL";
-		if(value.getClass() == double.class) return "REAL";
-		if(value.getClass() == float.class) return "REAL";
+		if(isDoubleNumber(value)) return "REAL";
 		
-		if(value instanceof Number) return "BIGINT";
+		if(value instanceof Number || value instanceof Boolean) return "BIGINT";
 		if(value instanceof Date) return "datetime";
 
-        return "VARCHAR(200)";		
+        return VARCHAR;		
+	}
+	
+	private static boolean isDoubleNumber(Object value) {
+		if(value == null) return false;
+		
+		if(value instanceof Double) return true;
+		if(value instanceof Float) return true;
+		if(value.getClass() == double.class) return true;
+		if(value.getClass() == float.class) return true;
+		
+		return false;
 	}
 }
