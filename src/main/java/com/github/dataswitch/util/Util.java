f0502ce337efc8cf1d6fc953f8ff44a162414d03
badqiu <badqiu@qq.com>
2022-11-03 19:30:32 +0800

    1

--- src/main/java/com/github/dataswitch/util/Util.java
+++ src/main/java/com/github/dataswitch/util/Util.java
@@ -11,6 +11,8 @@ import org.springframework.util.StringUtils;
 public class Util {
 
 	public static String[] splitColumns(String columns) {
+		if(columns == null) return null;
+		
 		String[] result = StringUtils.trimArrayElements(columns.trim().split("[,\\s]+"));
 		return result;
 	}
