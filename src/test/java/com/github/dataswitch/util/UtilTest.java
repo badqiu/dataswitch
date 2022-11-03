79eb3c0687a6739053ab28800eb5eac5c1f8303d
badqiu <badqiu@qq.com>
2022-11-03 19:29:52 +0800

    1

--- src/test/java/com/github/dataswitch/util/UtilTest.java
+++ src/test/java/com/github/dataswitch/util/UtilTest.java
@@ -12,6 +12,7 @@ public class UtilTest {
 		assertEquals("user;age",StringUtils.join(Util.splitColumns("user,age"),";"));
 		assertEquals("user;age",StringUtils.join(Util.splitColumns("user  age"),";"));
 		assertEquals("user;age;diy;blog;abc",StringUtils.join(Util.splitColumns("user\nage\n\t\ndiy\nblog,abc"),";"));
+		assertEquals("user;age;diy;blog;abc",StringUtils.join(Util.splitColumns("user\nage\n\t\n\t\t diy\nblog,abc"),";"));
 		assertEquals("user;age;diy;blog;abc",StringUtils.join(Util.splitColumns("user ,\nage,\n\t\ndiy,\nblog,abc"),";"));
 		
 		assertEquals("tdate;tdate_type;app_id;channel_id;pay;pay_account_idcnt;pay_device_idcnt;active_device_idcnt;active_account_idcnt",StringUtils.join(Util.splitColumns("				tdate,tdate_type,app_id,channel_id,\n				pay,pay_account_idcnt,pay_device_idcnt,active_device_idcnt,active_account_idcnt\n\t\n\t \n"),";"));
