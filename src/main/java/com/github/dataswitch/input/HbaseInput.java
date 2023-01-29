0471748787251f8a989cad626e8809741a7f46c1
badqiu <badqiu@qq.com>
2023-01-29 15:24:21 +0800

    1

---
+++ src/main/java/com/github/dataswitch/input/HbaseInput.java
@@ -0,0 +1,143 @@
+package com.github.dataswitch.input;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.Scan.ReadType;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.dataswitch.enums.Constants;
+import com.github.dataswitch.support.HbaseProvider;
+import com.github.dataswitch.util.InputOutputUtil;
+
+public class HbaseInput  extends HbaseProvider implements Input{
+	private static Logger logger = LoggerFactory.getLogger(HbaseInput.class);
+	
+    private String startKey = null;
+    private String endKey = null;
+    
+    private String columnFamily = null;
+
+    protected String encoding = StandardCharsets.UTF_8.name();
+    protected int scanCacheSize = Constants.DEFAULT_BUFFER_SIZE;
+    protected int  scanBatchSize = Constants.DEFAULT_BUFFER_SIZE;
+
+    protected Result _lastResult = null;
+    protected Scan _scan;
+    protected ResultScanner _resultScanner;
+
+    protected Table _hbaseTable;
+    private byte[] _startKey = null;
+    private byte[] _endKey = null;
+    private Charset _charset = null;
+    
+
+    protected void initScan(Scan scan) {
+    };
+
+    public void prepare() throws Exception {
+        this._scan = new Scan();
+        
+        //this._scan.setSmall(false);
+        this._scan.setReadType(ReadType.PREAD);
+        
+        this._scan.withStartRow(_startKey);
+        this._scan.withStopRow(_endKey);
+        this._scan.addFamily(Bytes.toBytes(columnFamily));
+        
+        logger.info("The task set startRowkey=[{}], endRowkey=[{}].", this.startKey, this.endKey);
+        //scan的Caching Batch全部留在hconfig中每次从服务器端读取的行数，设置默认值未256
+        this._scan.setCaching(this.scanCacheSize);
+        //设置获取记录的列个数，hbase默认无限制，也就是返回所有的列,这里默认是100
+        this._scan.setBatch(this.scanBatchSize);
+        //为是否缓存块，hbase默认缓存,同步全部数据时非热点数据，因此不需要缓存
+        this._scan.setCacheBlocks(false);
+        initScan(this._scan);
+
+        this._resultScanner = this._hbaseTable.getScanner(this._scan);
+    }
+
+    @Override
+	public void close() throws Exception {
+        InputOutputUtil.close(this._resultScanner);
+        InputOutputUtil.close(this._hbaseTable);
+    }
+
+    protected Result getNextHbaseRow()  {
+        try {
+			return getNextHbaseRow0();
+		} catch (IOException e) {
+			throw new RuntimeException("getNextHbaseRow error",e);
+		}
+    }
+
+	private Result getNextHbaseRow0() throws IOException {
+		Result result;
+        try {
+            result = _resultScanner.next();
+        } catch (IOException e) {
+            if (_lastResult != null) {
+                this._scan.setStartRow(_lastResult.getRow());
+            }
+            _resultScanner = this._hbaseTable.getScanner(_scan);
+            result = _resultScanner.next();
+            if (_lastResult != null && Bytes.equals(_lastResult.getRow(), result.getRow())) {
+                result = _resultScanner.next();
+            }
+        }
+        _lastResult = result;
+        // may be null
+        return result;
+	}
+
+    
+	@Override
+	public List<Object> read(int size) {
+		List<Object> list = new ArrayList<Object>(size);
+		for(int i = 0; i < size; i++) {
+			Result result = getNextHbaseRow();
+			if(result == null) {
+				break;
+			}
+			
+			Map row = result2Map(result);
+			if(row == null) {
+				continue;
+			}
+			
+			list.add(row);
+			
+			if(list.size() > size) {
+				break;
+			}
+		}
+		return list;
+	}
+	
+	protected Map result2Map(Result result) {
+		if(result == null) return null;
+		
+		return result.getFamilyMap(Bytes.toBytes(columnFamily));
+	}
+
+	@Override
+	public void open(Map<String, Object> params) throws Exception {
+		this._hbaseTable = getTable(getHbaseConfig(),getTable(),1);
+
+        this._charset = Charset.forName(encoding);
+        this._startKey = objectToHbaseBytes(startKey, _charset);
+        this._endKey = objectToHbaseBytes(endKey, _charset);
+	}
+	
+
+}
