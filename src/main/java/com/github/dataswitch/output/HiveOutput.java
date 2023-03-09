cd6de9cb14044a59c653fd76f7326b45d5cd7ebe
badqiu <badqiu@qq.com>
2023-03-09 10:20:42 +0800

    1

--- src/main/java/com/github/dataswitch/output/HiveOutput.java
+++ src/main/java/com/github/dataswitch/output/HiveOutput.java
@@ -2,11 +2,15 @@ package com.github.dataswitch.output;
 
 public class HiveOutput {
 
-	private String defaultFS;
-	private String fileType;
+	private String defaultFS; // hdfs://xxx:port
+	private String fileType; //orc,text
 	private String path;
 	private String fileName;
 	private String columns;
 	private String writeMode; //append,truncate,nonConflict
-	private String compress; //gzip,snappy
+	private String compress; //none,gzip,snappy
+	private String fieldDelimiter;
+	private String hadoopConfig;
+	private String encoding = "UTF-8";
+	
 }
