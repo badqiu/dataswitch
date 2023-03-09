f04fad2377d9aa1a062f67030db0ce8889355f31
badqiu <badqiu@qq.com>
2023-03-09 10:33:47 +0800

    1

--- src/test/java/com/github/dataswitch/VTest.java
+++ src/test/java/com/github/dataswitch/VTest.java
@@ -3,12 +3,14 @@ package com.github.dataswitch;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.junit.Test;
 
 public class VTest {
 
 	volatile static int count = 0;
+	static AtomicLong acount = new AtomicLong();
 	
 	@Test
 	public void testArray() {
@@ -28,6 +30,7 @@ public class VTest {
 				public void run() {
 					for(int i = 0; i < loopCount; i++) {
 						count++;
+						acount.incrementAndGet();
 					}
 					countDown.countDown();
 				}
@@ -35,8 +38,9 @@ public class VTest {
 			t.start();
 		}
 		countDown.await();
-		Thread.sleep(1000);
+//		Thread.sleep(1000);
 		System.out.println(count);
+		System.out.println(acount);
 	}
 	
 }
