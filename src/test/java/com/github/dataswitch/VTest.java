e8cbe9d4eccc6df217403b6ae91b981cd392fa17
badqiu <badqiu@qq.com>
2023-03-30 18:10:43 +0800

    1

--- src/test/java/com/github/dataswitch/VTest.java
+++ src/test/java/com/github/dataswitch/VTest.java
@@ -1,12 +1,16 @@
 package com.github.dataswitch;
 
 import java.io.IOException;
+import java.math.BigInteger;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.time.DateFormatUtils;
 import org.junit.Test;
 
 public class VTest {
@@ -14,6 +18,34 @@ public class VTest {
 	volatile static int count = 0;
 	static AtomicLong acount = new AtomicLong();
 	
+	@Test
+	public void test_device() throws IOException {
+		DecimalFormat df = new DecimalFormat("##.##");
+		double r = Double.MAX_VALUE;
+		for(int i = 0; i < 15; i++) {
+			r = r / Long.MAX_VALUE ;
+		}
+		
+		System.out.println("value: "+df.format(r));
+		System.out.println("double,value: "+df.format(Double.MAX_VALUE));
+		System.out.println("float,value: "+df.format(Float.MAX_VALUE));
+		System.out.println("double/long,value: "+df.format(Double.MAX_VALUE/Long.MAX_VALUE));
+		System.out.println("double/float,value: "+df.format(Double.MAX_VALUE/Float.MAX_VALUE));
+		System.out.println("BigInteger,value: "+df.format(new BigInteger("1797693134862315700000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000022222200000000000000000000000000000000000000000000000000000000000000000001111")));
+		
+		String id = genId();
+		System.out.println(id);
+		System.out.println(df.format(new BigInteger(id)));
+	}
+	
+	long seq = 0;
+	String workId = "99900001";
+	private String genId() {
+		String time = DateFormatUtils.format(new Date(), "yyyyMMddHHmmssSSS");
+		long incr = ++seq;
+		return time+workId+incr;
+	}
+
 	@Test
 	public void test_exec() throws IOException {
 		Process p1 = Runtime.getRuntime().exec("bin/flume-ng agent --name usbyteplus1 --conf conf/ --conf-file conf/usbyteplus1.conf -Dflume.root.logger=INFO");
