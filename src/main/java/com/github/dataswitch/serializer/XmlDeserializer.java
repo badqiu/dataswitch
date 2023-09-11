474000d0f3fff3cd63093a6f203b0d5a996f381a
badqiu <badqiu@qq.com>
2023-09-11 19:15:47 +0800

    1

--- src/main/java/com/github/dataswitch/serializer/XmlDeserializer.java
+++ src/main/java/com/github/dataswitch/serializer/XmlDeserializer.java
@@ -1,8 +1,15 @@
 package com.github.dataswitch.serializer;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.WeakHashMap;
 
+import org.apache.commons.lang.StringUtils;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.springframework.core.serializer.Deserializer;
 
 import com.github.dataswitch.BaseObject;
@@ -11,10 +18,39 @@ import com.thoughtworks.xstream.XStream;
 public class XmlDeserializer extends BaseObject implements Deserializer{
 	static XStream xstream = new XStream();
 	
+
+	private Map<InputStream,BufferedReader> readers = new WeakHashMap<InputStream,BufferedReader>();
+	
+//	@Override
+//	public Object deserialize(InputStream inputStream) throws IOException {
+//		Object result = xstream.fromXML(inputStream);
+//		return result;
+//	}
+	
 	@Override
-	public Object deserialize(InputStream inputStream) throws IOException {
-		Object result = xstream.fromXML(inputStream);
-		return result;
+	public Map deserialize(InputStream inputStream) throws IOException {
+		BufferedReader reader = toBufferedReader(inputStream);
+		String line = null;
+		while((line = reader.readLine()) != null) {
+			if(StringUtils.isBlank(line)) {
+				continue;
+			}
+			return (Map)deserializeLine(line);
+		}
+		return null;
 	}
 
+	protected Object deserializeLine(String xml) throws IOException, JsonParseException, JsonMappingException {
+		return xstream.fromXML(xml);
+	}
+
+	private BufferedReader toBufferedReader(InputStream inputStream) {
+		BufferedReader reader = readers.get(inputStream);
+		if(reader == null) {
+			reader = new BufferedReader(new InputStreamReader(inputStream));
+			readers.put(inputStream, reader);
+		}
+		return reader;
+	}
+	
 }
