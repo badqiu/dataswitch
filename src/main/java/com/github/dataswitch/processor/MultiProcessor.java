b85e496579de4639c7637e73f690d98ee4b865a7
badqiu <badqiu@qq.com>
2023-09-13 17:18:09 +0800

    1

--- src/main/java/com/github/dataswitch/processor/MultiProcessor.java
+++ src/main/java/com/github/dataswitch/processor/MultiProcessor.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.ArrayUtils;
 
 import com.github.dataswitch.Enabled;
 import com.github.dataswitch.util.InputOutputUtil;
@@ -44,6 +45,8 @@ public class MultiProcessor implements Processor{
 	
 	@Override
 	public List<Object> process(List<Object> datas) throws Exception {
+		if(ArrayUtils.isEmpty(processors)) return datas;
+		
 		List<Object> tempDatas = datas;
 		for(Processor p : processors) {
 			tempDatas = p.process(tempDatas);
