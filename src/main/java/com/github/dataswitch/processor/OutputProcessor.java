7b2e5248fde7166a0470ffac2cdba82ee3b14786
badqiu <badqiu@qq.com>
2023-09-13 16:59:00 +0800

    1

--- src/main/java/com/github/dataswitch/processor/OutputProcessor.java
+++ src/main/java/com/github/dataswitch/processor/OutputProcessor.java
@@ -1,6 +1,7 @@
 package com.github.dataswitch.processor;
 
 import java.util.List;
+import java.util.Map;
 
 import com.github.dataswitch.output.Output;
 import com.github.dataswitch.util.ObjectSqlQueryUtil;
@@ -29,5 +30,16 @@ public class OutputProcessor implements Processor {
 		output.write(datas);
 		return datas;
 	}
-
+	
+	@Override
+	public void open(Map<String, Object> params) throws Exception {
+		Processor.super.open(params);
+		output.open(params);
+	}
+	
+	@Override
+	public void close() throws Exception {
+		Processor.super.close();
+		output.close();
+	}
 }
