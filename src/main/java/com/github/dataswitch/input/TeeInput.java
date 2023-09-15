1284e0b4fe0a4e05c551b57fcd086854fa375db4
badqiu <badqiu@qq.com>
2023-09-15 15:01:06 +0800

    1

--- src/main/java/com/github/dataswitch/input/TeeInput.java
+++ src/main/java/com/github/dataswitch/input/TeeInput.java
@@ -1,8 +1,9 @@
 package com.github.dataswitch.input;
 
 import java.util.List;
+import java.util.Map;
 
-import com.github.dataswitch.BaseObject;
+import com.github.dataswitch.enums.FailMode;
 import com.github.dataswitch.output.Output;
 import com.github.dataswitch.util.IOUtil;
 
@@ -14,27 +15,67 @@ import com.github.dataswitch.util.IOUtil;
  */
 public class TeeInput extends ProxyInput{
 
-	private Output branch;
+	private Output[] branchs;
+	
 	 /**
      * Flag for closing also the associated output stream when this
      * stream is closed.
      */
-    private final boolean closeBranch;
+    private boolean closeBranch;
     
-    public TeeInput(Input input,Output branch) {
-    	this(input,branch,false);
+    private FailMode failMode = FailMode.FAIL_FAST;
+    
+    public TeeInput() {
     }
     
-	public TeeInput(Input proxy,Output branch,boolean closeBranch) {
+    public TeeInput(Input input,Output branch) {
+    	this(input,false,branch);
+    }
+    
+	public TeeInput(Input proxy,boolean closeBranch,Output... branchs) {
 		super(proxy);
-		this.branch = branch;
+		this.branchs = branchs;
 		this.closeBranch = closeBranch;
+	}
+	
+	public Output[] getBranchs() {
+		return branchs;
+	}
+
+	public void setBranchs(Output... branchs) {
+		this.branchs = branchs;
+	}
+
+	public void setBranch(Output branch) {
+		setBranchs(branch);
+	}
+	
+	public FailMode getFailMode() {
+		return failMode;
+	}
+
+	public void setFailMode(FailMode failMode) {
+		this.failMode = failMode;
+	}
+
+	public boolean isCloseBranch() {
+		return closeBranch;
+	}
+
+	@Override
+	public void open(Map<String, Object> params) throws Exception {
+		super.open(params);
+		openBranchs(params);
 	}
 
 	@Override
 	public List<Object> read(int size) {
 		List<Object> result =  super.read(size);
-		branch.write(result);
+		
+		failMode.forEach(branchs, branch -> {
+			branch.write(result);
+		});
+		
 		return result;
 	}
 	
@@ -44,8 +85,24 @@ public class TeeInput extends ProxyInput{
 			super.close();
 		}finally {
 			if(closeBranch) {
-				IOUtil.closeQuietly(branch);
+				closeBranchs();
 			}
 		}
+	}
+
+	private void openBranchs(Map<String, Object> params) {
+		failMode.forEach(branchs, branch -> {
+			try {
+				branch.open(params);
+			} catch (Exception e) {
+				throw new RuntimeException("open error on output:"+branch+" params:"+params,e);
+			}
+		});
+	}
+	
+	private void closeBranchs() {
+		failMode.forEach(branchs, branch -> {
+			IOUtil.close(branch);
+		});
 	}
 }
