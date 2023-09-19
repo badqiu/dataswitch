d13c205117826310b7dbf60bf4865ae396854e3a
badqiu <badqiu@qq.com>
2023-09-19 14:59:00 +0800

    1

--- src/main/java/com/github/dataswitch/runner/param/InputParameterGenerator.java
+++ src/main/java/com/github/dataswitch/runner/param/InputParameterGenerator.java
@@ -26,6 +26,30 @@ public class InputParameterGenerator implements Supplier<Map<String,Object>>{
 	public void setInput(Input input) {
 		this.input = input;
 	}
+	
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getDefaultValue() {
+		return defaultValue;
+	}
+
+	public void setDefaultValue(String defaultValue) {
+		this.defaultValue = defaultValue;
+	}
 
 	@Override
 	public Map<String, Object> get() {
