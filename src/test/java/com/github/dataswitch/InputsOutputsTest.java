4487af7e15a6eafa5be8a5bd6fef9cee344f5cf0
qiubaichao <qiubaichao@lizhi.fm>
2023-10-19 21:47:49 +0800

    1

--- src/test/java/com/github/dataswitch/InputsOutputsTest.java
+++ src/test/java/com/github/dataswitch/InputsOutputsTest.java
@@ -2,10 +2,12 @@ package com.github.dataswitch;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import org.aspectj.lang.annotation.After;
 import org.junit.Test;
 
 import com.github.dataswitch.input.Input;
@@ -34,6 +36,35 @@ public class InputsOutputsTest {
 	public void test_async() {
 		job.setAsync(true);
 		
+		initInputOutput();
+		
+		job.exec();
+		
+		assertEquals(100,count);
+		assertEquals(99,writeCount);
+		
+	}
+
+	@Test
+	public void testSyncWithBatch() {
+		initInputOutput();
+		job.setSync(true);
+		job.setBatchSize(50);
+		job.exec();
+		assertEquals(100,count);
+		assertEquals(99,writeCount);
+	}
+
+	@Test
+	public void testSync() {
+		initInputOutput();
+		job.setSync(true);
+		job.exec();
+		assertEquals(100,count);
+		assertEquals(99,writeCount);
+	}
+
+	private void initInputOutput() {
 		job.setInput(new Input() {
 			@Override
 			public List<Map<String, Object>> read(int size) {
@@ -50,31 +81,18 @@ public class InputsOutputsTest {
 			@Override
 			public void write(List<Map<String, Object>> rows) {
 				for(Object row : rows) {
-					System.out.println(row+" writeCount:"+writeCount+" count:"+count);
+//					System.out.println(row+" writeCount:"+writeCount+" count:"+count+" rows.size:"+rows.size());
 					writeCount++;
 				}
+				System.out.println(" -------- rows.size:"+rows.size());
 			}
 		});
-		
-		job.exec();
-		
-		assertEquals(100,count);
-		assertEquals(99,writeCount);
-		
-		
-		count = 0;
-		job.setSync(true);
-		job.exec();
-		assertEquals(100,count);
-		assertEquals(198,writeCount);
-		
-		
-		count = 0;
-		job.setSync(true);
-		job.setBatchSize(50);
-		job.exec();
-		assertEquals(100,count);
-		assertEquals(297,writeCount);
+	}
+	
+	@org.junit.After
+	public void after() throws Exception {
+		job.close();
+		Thread.sleep(1000 * 3);
 	}
 
 }
