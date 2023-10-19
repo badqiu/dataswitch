89fc5ea4399d9b8cdb1fe732b6d4a4ed9666db72
qiubaichao <qiubaichao@lizhi.fm>
2023-10-19 22:21:30 +0800

    1

--- src/main/java/com/github/dataswitch/output/ListOutput.java
+++ src/main/java/com/github/dataswitch/output/ListOutput.java
@@ -8,12 +8,12 @@ import org.apache.commons.collections.CollectionUtils;
 
 public class ListOutput implements Output{
 
-	private List list = new ArrayList();
+	private List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 	
 	public ListOutput() {
 	}
 	
-	public ListOutput(List list) {
+	public ListOutput(List<Map<String, Object>> list) {
 		this.list = list;
 	}
 
@@ -24,7 +24,7 @@ public class ListOutput implements Output{
 		list.addAll(rows);
 	}
 
-	public List getList() {
+	public List<Map<String, Object>> getList() {
 		return list;
 	}
 
